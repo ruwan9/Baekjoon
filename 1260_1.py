814a6cd437863c0a4c0f810a28cfc3ee9e8505b6
ruwan9 <ruwan9@kakao.com>
2022-01-21 00:51:47 +0900

    1260: DFS와 BFS

---
+++ 1260_1.py
@@ -0,0 +1,43 @@
+import sys
+sys.stdin = open('inputs/1260.txt')
+from collections import deque
+
+
+def dfs(graph, v, visited):
+    visited[v] = True
+    print(v, end=" ")
+
+    for i in sorted(graph[v]):
+        if visited[i] == False:
+            dfs(graph, i, visited)
+
+
+def bfs(graph, start, visited):
+    queue = deque([start])
+    visited[start] = True
+
+    while queue:
+        v = queue.popleft()
+        print(v, end=" ")
+
+        for i in sorted(graph[v]):
+            if visited[i] == False:
+                queue.append(i)
+                visited[i] = True
+
+
+N, M, V = map(int, input().split())
+visited = [False] * (N+1)
+graph = [[] for _ in range(N+1)]
+for _ in range(M):
+    n1, n2 = map(int, input().split())
+    graph[n1].append(n2)
+    graph[n2].append(n1)
+# print(graph)
+
+dfs(graph, V, visited)
+visited = [False] * (N+1)
+print()
+bfs(graph, V, visited)
+
+
---
+++ 1260_2.py
@@ -0,0 +1,41 @@
+import sys
+sys.stdin = open('inputs/1260.txt')
+from collections import deque
+
+
+def bfs(start_v):
+  queue = deque()
+  discoverd = [start_v]
+  queue.append(start_v)
+
+  while queue:
+    v = queue.popleft()
+    print(v, end=' ')
+
+    for w in range(len(graph[start_v])):
+      if graph[v][w] == 1 and (w not in discoverd):
+        discoverd.append(w)
+        queue.append(w)
+
+
+def dfs(start_v, discoverd=[]):
+  discoverd.append(start_v)
+  print(start_v, end=' ')
+
+  for w in range(len(graph[start_v])):
+    if graph[start_v][w] == 1 and (w not in discoverd):
+      dfs(w, discoverd)
+
+
+N, M, V = map(int, input().split())
+graph = [[0] * (N + 1) for _ in range(N + 1)]
+# print(graph)
+for _ in range(M):
+  m1, m2 = map(int, input().split())
+  # 노드 연결 하기
+  graph[m1][m2] = graph[m2][m1] = 1
+print(graph)
+
+dfs(V)
+print()
+bfs(V)
