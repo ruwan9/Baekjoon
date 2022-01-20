2c0bdd5300984bc0d97a70e5df7353790e989e97
ruwan9 <ruwan9@kakao.com>
2022-01-21 01:54:07 +0900

    11724: 연결 요소의 개수

---
+++ 11724_1.py
@@ -0,0 +1,28 @@
+import sys
+sys.stdin = open('inputs/11724.txt')
+
+
+def dfs(v):
+    visited[v] = True
+
+    for i in graph[v]:
+        if visited[i] == False:
+            dfs(i)
+
+
+N, M = map(int, input().split())
+graph = [[] for _ in range(N+1)]
+visited = [False] * (N+1)
+cnt = 0
+for _ in range(M):
+    u, v = map(int, input().split())
+    graph[u].append(v)
+    graph[v].append(u)
+
+
+for i in range(1, N+1):
+    if visited[i] == False:
+        cnt += 1
+        dfs(i)
+
+print(cnt)
---
+++ 11724_2.py
@@ -0,0 +1,30 @@
+import sys
+sys.stdin = open('inputs/11724.txt')
+# sys.setrecursionlimit(100000)
+
+
+def dfs(now):
+    for nxt in range(N):
+        if adj[now][nxt] and not chk[nxt]:
+            chk[nxt] = True
+            dfs(nxt)
+
+
+N, M = map(int, input().split())
+# 인접행렬
+adj = [[0] * N for _ in range(N)]
+for _ in range(M):
+    a, b = map(lambda x: x-1, map(int, input().split()))  # 1씩 빼서 0부터 시작
+    adj[a][b] = adj[b][a] = 1
+print(adj)
+
+ans = 0
+chk = [False] * N
+
+for i in range(N):
+    if chk[i] == False:
+        ans += 1
+        chk[i] = True
+        dfs(i)
+
+print(ans)
