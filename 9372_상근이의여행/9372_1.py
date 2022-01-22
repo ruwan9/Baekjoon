e8d3587905887b289d5dfe349ef51f9023514edc
ruwan9 <ruwan9@kakao.com>
2022-01-22 12:30:36 +0900

    9372: 상근이의 여행

---
+++ 9372_상근이의여행/9372_1.py
@@ -0,0 +1,33 @@
+import sys
+sys.stdin = open('../inputs/9372.txt')
+
+
+def dfs(v):
+    global cnt
+    visited[v] = True
+
+    for i in graph[v]:
+        if visited[i] == False:
+            dfs(i)
+            cnt += 1
+
+
+T = int(input())
+for _ in range(T):
+    N, M = map(int, input().split())
+    graph = [[] for _ in range(N+1)]
+    visited = [False] * (N+1)
+    cnt = 0
+
+    for _ in range(M):
+        a, b = map(int, input().split())
+        graph[a].append(b)
+        graph[b].append(a)
+    print(graph)
+    print(visited)
+
+    for i in range(1, N+1):
+        if visited[i] == False:
+            dfs(i)
+
+    print(cnt)
---
+++ 9372_상근이의여행/9372_2.py
@@ -0,0 +1,26 @@
+import sys
+sys.stdin = open('../inputs/9372.txt')
+
+
+def dfs(idx, cnt):
+    visited[idx] = 1
+
+    for i in graph[idx]:
+        if not visited[i]:
+            cnt = dfs(i, cnt+1)
+
+    return cnt
+
+
+T = int(input())
+for _ in range(T):
+    N, M = map(int, input().split())
+    graph = [[] for _ in range(N+1)]
+    visited = [False] * (N+1)
+
+    for _ in range(M):
+        a, b = map(int, input().split())
+        graph[a].append(b)
+        graph[b].append(a)
+
+    print(dfs(1, 0))
