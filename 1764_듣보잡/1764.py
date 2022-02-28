face4e9ee65f9345857a0b08f5f8108fac282ad9
ruwan9 <ruwan9@kakao.com>
2022-02-28 23:57:20 +0900

    1764: 듣보잡

--- 1764_듣보잡/1764.py
+++ 1764_듣보잡/1764.py
@@ -2,16 +2,15 @@ import sys
 sys.stdin = open('input.txt')
 
 N, M = map(int, input().split())
-people = list(input() for _ in range(N+M))
-h = people[:N]
-s = people[N:]
-res = []
+h = set()
+for _ in range(N):
+    h.add(input())
+s = set()
+for _ in range(M):
+    s.add(input())
 
-for person in h:
-    if person in s:
-        res.append(person)
+res = sorted(list(h & s))
 
-res.sort()
 print(len(res))
 for i in res:
     print(i)
