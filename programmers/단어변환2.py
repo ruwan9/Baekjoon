05a987f19d6a9143285af449be9bb867e25f8345
ruwan9 <ruwan9@kakao.com>
2022-03-04 15:56:26 +0900

    단어변환

--- programmers/단어변환2.py
+++ programmers/단어변환2.py
@@ -1,23 +1,6 @@
-from collections import deque
-
-
-def bfs(begin):
-    global cnt
-    queue = deque([begin])
-
-    while queue:
-        start = queue.popleft()
-
-        if start == target:
-            cnt += 1
-            break
-
-        for i in range(len(words)):
-            if check(start, words[i]) and visited[i] == 0:
-                cnt += 1
-                queue.append(words[i])
-                visited[i] = 1
-                print(start, words[i], visited, queue)
+begin = "hit"
+target = "cog"
+words = ["hot", "dot", "dog", "lot", "log", "cog"]
 
 
 def check(w1, w2):
@@ -29,12 +12,25 @@ def check(w1, w2):
     return True if tmp == 1 else False
 
 
-begin = "hit"
-target = "cog"
-words = ["hot", "dot", "dog", "lot", "log", "cog"]
-cnt = 0
-visited = [0 for _ in range(len(words))]
-print(visited)
+def solution(begin, target, words):
+    answer = 0
 
-bfs(begin)
-print(cnt)
+    if target not in words:
+        return 0
+
+    visited = [0] * len(words)
+    queue = [begin]
+
+    while queue:
+        word = queue.pop()
+        if word == target:
+            return answer
+
+        for i in range(len(words)):
+            if visited[i] == 0 and check(word, words[i]):
+                visited[i] = 1
+                queue.append(words[i])
+        answer += 1
+
+
+print(solution(begin, target, words))
