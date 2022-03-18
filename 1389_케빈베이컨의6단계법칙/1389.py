6f89f85ad1fdf8be0c9bff3365f2148813e0d251
ruwan9 <ruwan9@kakao.com>
2022-03-19 01:16:56 +0900

    케빈 베이컨의 6단계 법칙

--- 1389_케빈베이컨의6단계법칙/1389.py
+++ 1389_케빈베이컨의6단계법칙/1389.py
@@ -6,26 +6,34 @@ from collections import deque
 def bfs(start):
     queue = deque()
     queue.append(start)
+    visited[start] = 1
+
 
     while queue:
         v = queue.popleft()
+        # print(visited)
         for i in data[v]:
             if visited[i] == 0:
+                # 단계(횟수?) 구하기
+                visited[i] = visited[v] + 1
                 queue.append(i)
-                visited[i] = 1
 
 
 N, M = map(int, input().split())
 data = [[] for _ in range(N+1)]
-visited = [0] * (N+1)
-res = []
-print(data)
-
 for _ in range(M):
     a, b = map(int, input().split())
     data[a].append(b)
     data[b].append(a)
 
-print(data)
-bfs(1)
-print(visited)
+# 케빈 베이컨 수
+zero = float('inf')
+res = [zero]  # 0번 인덱스 채우기
+
+for i in range(1, N+1):
+    visited = [0] * (N+1)
+    bfs(i)
+    # 유저 별 단계의 합 더해주기
+    res.append(sum(visited) - visited[i])
+
+print(res.index(min(res)))
